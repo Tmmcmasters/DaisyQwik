70355da512f653bc2c325e8473e9aa965f99eaf1
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2024-01-13 13:25:30 -0500

    Added DaisyUi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
   },
-  plugins: [],
 };
