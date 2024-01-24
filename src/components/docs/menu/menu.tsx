6fd9cafe30560c0c824f033ffbfe58105a99d201
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2024-01-23 22:49:04 -0500

    Added icon for docs

--- src/components/docs/menu/menu.tsx
+++ src/components/docs/menu/menu.tsx
@@ -6,6 +6,9 @@ export const Menu = component$(() => {
       <li>
         <details open>
           <summary>
+            <span>
+          <svg width="18" height="18" viewBox="0 0 48 48" class="text-orange-400 h-5 w-5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 7H16C20.4183 7 24 10.5817 24 15V42C24 38.6863 21.3137 36 18 36H5V7Z" fill="none" stroke="currentColor" stroke-width="4" stroke-linejoin="bevel"></path><path d="M43 7H32C27.5817 7 24 10.5817 24 15V42C24 38.6863 26.6863 36 30 36H43V7Z" fill="none" stroke="currentColor" stroke-width="4" stroke-linejoin="bevel"></path></svg>
+            </span>
             <a href="/docs">Docs</a>
           </summary>
           <ul>
